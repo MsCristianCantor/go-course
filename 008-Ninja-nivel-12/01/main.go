595c85fccbb154d5138f115a9b1241e3a3fdb2c3
Cristian Cantor <ccantor@merqueo.com>
2022-06-29 10:41:32 -0500

    [fix] solved error

--- 008-Ninja-nivel-12/01/main.go
+++ 008-Ninja-nivel-12/01/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/MsCristianCantor/go-course/008-Ninja-nivel-12/01/perro"
 )
 
 type canino struct {
@@ -12,7 +14,7 @@ type canino struct {
 func main() {
 	c1 := canino{
 		nombre: "Chester",
-		// edad:   perro.Edad(2),
+		edad:   perro.Edad(1),
 	}
 	fmt.Println(c1)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MsCristianCantor/go-course
+
+go 1.17
